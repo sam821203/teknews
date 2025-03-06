e816af66ad93b2e9feebf9f626dbd4bb4e5bcdbf
Sam Huang (黃宇浩) <sam821203@yahoo.com.tw>
2025-03-06 10:39:02 +0800

    feat: 新增notifications模組相關檔案

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from "@angular/core"
 import { RouterOutlet } from "@angular/router"
 import { WeatherModule } from "./weather/weather.module"
+import { NotificationsModule } from "./notifications/notifications.module"
 
 @Component({
   selector: "app-root",
-  imports: [RouterOutlet, WeatherModule],
+  imports: [RouterOutlet, WeatherModule, NotificationsModule],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
 })
---
+++ src/app/notifications/notification-list/notification-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notification-list',
+  standalone: false,
+  templateUrl: './notification-list.component.html',
+  styleUrl: './notification-list.component.scss'
+})
+export class NotificationListComponent {
+
+}
