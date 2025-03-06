3fd5508f9eaa1f32660fdcf1cf46b057d85bec60
Sam Huang (黃宇浩) <sam821203@yahoo.com.tw>
2025-03-06 15:04:56 +0800

    feat: 新增success notification樣式與關閉功能

--- src/app/notifications/notification-list/notification-list.component.ts
+++ src/app/notifications/notification-list/notification-list.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core"
+import { Command, NotificationsService } from "../notifications.service"
+import { Observable } from "rxjs"
 
 @Component({
-  selector: 'app-notification-list',
+  selector: "app-notification-list",
   standalone: false,
-  templateUrl: './notification-list.component.html',
-  styleUrl: './notification-list.component.scss'
+  templateUrl: "./notification-list.component.html",
+  styleUrl: "./notification-list.component.scss",
 })
-export class NotificationListComponent {
+export class NotificationListComponent implements OnInit {
+  messages!: Observable<Command[]>
 
+  constructor(private notificationsService: NotificationsService) {
+    this.messages = this.notificationsService.messagesOutput
+    setInterval(() => {
+      this.notificationsService.addSuccess("Success message")
+    }, 2000)
+  }
+
+  ngOnInit(): void {}
+
+  clearMessage(id: number) {
+    this.notificationsService.clearMessage(id)
+  }
 }
--- src/app/notifications/notifications.service.ts
+++ src/app/notifications/notifications.service.ts
@@ -1,9 +1,54 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core"
+import { scan } from "rxjs/operators"
+import { Observable, Subject } from "rxjs"
+
+export interface Command {
+  id: number
+  type: "success" | "error" | "clear"
+  text?: string
+}
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class NotificationsService {
+  messagesInput: Subject<Command>
+  messagesOutput: Observable<Command[]>
 
-  constructor() { }
+  constructor() {
+    this.messagesInput = new Subject<Command>()
+    this.messagesOutput = this.messagesInput.pipe(
+      scan((messages: Command[], command: Command) => {
+        if (command.type === "clear") {
+          return messages.filter((message) => message.id !== command.id)
+        } else {
+          return [...messages, command]
+        }
+      }, [])
+    )
+  }
+
+  addSuccess(message: string) {
+    const id = this.randomId()
+    this.messagesInput.next({ id, type: "success", text: message })
+    setTimeout(() => {
+      this.clearMessage(id)
+    }, 5000)
+  }
+
+  addError(message: string) {
+    const id = this.randomId()
+    this.messagesInput.next({ id, type: "error", text: message })
+    setTimeout(() => {
+      this.clearMessage(id)
+    }, 5000)
+  }
+
+  clearMessage(id: number) {
+    this.messagesInput.next({ id, type: "clear" })
+  }
+
+  private randomId() {
+    return Math.round(Math.random() * 10000)
+  }
 }
