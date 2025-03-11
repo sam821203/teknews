a34d3376e1f440b9ce579859ee9026ddd371d717
Sam Huang (黃宇浩) <sam821203@yahoo.com.tw>
2025-03-11 17:19:13 +0800

    fix: 確保目錄存在

--- create-env.js
+++ create-env.js
@@ -1,7 +1,16 @@
+require("dotenv").config()
+
 const fs = require("fs")
+const path = "./src/environments"
+
+if (!fs.existsSync(path)) {
+  fs.mkdirSync(path, { recursive: true })
+}
+
 const envConfig = `export const environment = {
   production: true,
   apiKey: "${process.env.API_KEY}",
 };`
+
 fs.writeFileSync("./src/environments/environment.prod.ts", envConfig)
 console.log("✅ environment.prod.ts created!")
