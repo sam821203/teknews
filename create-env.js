f859ebf9ba3d84c1c7c2a89021ffe31fd7061390
Sam Huang (黃宇浩) <sam821203@yahoo.com.tw>
2025-03-12 11:40:12 +0800

    fix: 補上目錄資料夾判斷

--- create-env.js
+++ create-env.js
@@ -1,5 +1,10 @@
 const fs = require("fs")
 const dotenv = require("dotenv").config({ path: "src/.env" })
+const path = "./src/environments"
+
+if (!fs.existsSync(path)) {
+  fs.mkdirSync(path, { recursive: true })
+}
 
 const envFile = `export const environment = {
     API_KEY: '${process.env.API_KEY}',
